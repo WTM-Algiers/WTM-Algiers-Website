f98509741bedb30782391c8d43d4fabcea85e434
Abderraouf GATTAL <gattal.raouf@gmail.com>
2019-12-22 23:50:35 +0100

    reformatting and fixing some issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import logo from "../images/logo/WTM-Main.svg"
 import SM from "../components/socialmedia"
@@ -11,13 +9,12 @@ const IndexPage = () => (
   <Layout>
     <SEO description="Women TechMakers Algiers / WTM Algiers Official Website" title="WTM Algiers Website Main Home" />
     <div style={{ maxWidth: `400px`, marginBottom: `0.5rem` }}>
-      <img src={logo} alt={"WTM Logo"}/>
+      <img src={logo} alt={"WTM Logo"} />
     </div>
     <p>Welcome to WTM Algiers Official Website.</p>
     <p>We are still working on it cuerrently, See you Soon ;)</p>
 
-    <SM direction=""/>
-    
+    <SM direction="" />
 
   </Layout>
 )
