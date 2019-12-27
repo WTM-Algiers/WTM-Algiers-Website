4929f776d945e618a8c90201532c4f561984d18b
aminatadjer <ga_tadjer@esi.dz>
2019-12-27 02:31:54 +0100

    404 component done

---
+++ src/components/404component.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./404component.css";
+import Wtm_main from "../images/logo/WTM-Main.svg";
+import image404 from "../images/404.svg";
+import { Link } from "gatsby";
+
+const NotFound404 = () => {
+  return (
+    <div class="container">
+      <img
+        src={Wtm_main}
+        alt="Wtm logo"
+        className="back"
+        style={{ minidth: `400px`, marginBottom: `0.5rem` }}
+      />
+      <div className="center">
+        <img src={image404} />
+        <div className="text-message">
+          It appears that you used an invalid link press
+          <Link className="text-style-1" to="./">
+            {" "}
+            here{" "}
+          </Link>{" "}
+          to go to the Main page{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound404;
