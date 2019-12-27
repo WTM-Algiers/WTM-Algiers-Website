ad5cbdb6efcb085c3c361775cc7d217c7cac8de3
EL_Kaouther River <hk_boulaouinat@esi.dz>
2019-12-27 14:18:56 +0100

    contactUs components

---
+++ src/components/contactUs.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './contactUs.css'
+const contactUs = () => (
+  <div className={'contact-form'}>
+    <form
+      name='contact-form'
+      method='post'
+      netlify-honeypot='bot-field'
+      data-netlify='true'
+    >
+      <input type='hidden' name='bot-field' />
+      <div className={'input-row'}>
+        <input name='name' placeholder='Full Name' type='text' id='name' />
+      </div>
+      <div className={'input-row'}>
+        <input name='email' placeholder='Email' type='email' id='email' />
+      </div>
+
+      <div className={'textarea-row'}>
+        <textarea
+          className={'textarea'}
+          name='message'
+          placeholder='Message ...'
+          id='message'
+        />
+      </div>
+      <div className={'button-row'}>
+        <button type='submit' className={'submitButton'}>
+          Send
+        </button>
+      </div>
+    </form>
+  </div>
+)
+export default contactUs
