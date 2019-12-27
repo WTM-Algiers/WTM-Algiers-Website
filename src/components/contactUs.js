4a8c19ef3b37cee76ae5ec2ce487475b3ca0c8ef
EL_Kaouther River <hk_boulaouinat@esi.dz>
2019-12-27 12:06:08 +0100

    contact Us components

---
+++ src/components/contactUs.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './contactUs.css'
+const contactUs = () => (
+  <div className={'contact-form'}>
+    <form
+      name='contact-form'
+      method='post'
+      netlify-honeypot='bot-field'
+      data-netlify='true'
+      action='#'
+    >
+      <input type='hidden' name='form-name' value='contact' />
+      <div className={'input-row'}>
+        <input name='name' placeholder='Full Name' type='text' id='name' />
+      </div>
+      <div className={'input-row'}>
+        <input name='email' placeholder='Email' type='email' id='email' />
+      </div>
+
+      <div className={'textarea-row'}>
+        <textarea
+          className={'textarea'}
+          name='message'
+          placeholder='Message ...'
+          id='message'
+        />
+      </div>
+      <div className={'button-row'}>
+        <button type='submit' className={'submitButton'}>
+          Send
+        </button>
+      </div>
+    </form>
+  </div>
+)
+export default contactUs
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,36 @@
+import React from 'react'
 
-import React from "react";
-
-import Layout from "../components/layout";
-import SEO from "../components/seo";
-import logo from "../images/logo/WTM-Main.svg";
-import SM from "../components/socialmedia";
-import LP from "../components/landing";
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import logo from '../images/logo/WTM-Main.svg'
+import SM from '../components/socialmedia'
+import LP from '../components/landing'
+import Form from '../components/contactUs'
 const IndexPage = () => (
   <Layout>
-    <SEO description="Women TechMakers Algiers / WTM Algiers Official Website" title="WTM Algiers Website Main Home" />
+    <SEO
+      description='Women TechMakers Algiers / WTM Algiers Official Website'
+      title='WTM Algiers Website Main Home'
+    />
 
-    <LP/>
+    <LP />
 
-    <div style={{ maxWidth: `400px`, marginBottom: `0.5rem`, display:"block", margin:"auto" }}>
-      <img src={logo} alt={"WTM Logo"}/>
+    <div
+      style={{
+        maxWidth: `400px`,
+        marginBottom: `0.5rem`,
+        display: 'block',
+        margin: 'auto'
+      }}
+    >
+      <img src={logo} alt={'WTM Logo'} />
     </div>
 
     <p>Welcome to WTM Algiers Official Website.</p>
     <p>We are still working on it cuerrently, See you Soon ;)</p>
 
-    <SM direction="" />
-
+    <SM direction='' />
+    <Form></Form>
   </Layout>
 )
 
