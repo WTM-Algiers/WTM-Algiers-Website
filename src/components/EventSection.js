0fbe1386299d5814a8d20dc7790120593a45b960
MeridjaNassim <ha_meridja@esi.dz>
2019-12-29 14:38:11 +0100

    Fixed Responsiveness , works on default screens

--- src/components/EventSection.js
+++ src/components/EventSection.js
@@ -7,15 +7,14 @@ import './EventSection.css'
 const EventSection =() => {
     return <section className="eventSection">
         <h1 className="title-section"><span id ="our">OUR</span> EVENTS</h1>
-        <div style={{width : "100%" , height:"30em" ,margin:"auto"}}>
-        <div className="slideshow">
-           <EventSlideShow></EventSlideShow>
-           </div>
-            <img src={greenShape} alt="greenshape" className="green-svg"></img>
-           <img src={blueShape} alt="blueshape" className="blue-svg"></img>
-         
-           
+        <div className ="section-container">
+            <div className="slideshow">
+                <EventSlideShow></EventSlideShow>
+            </div>   
         </div>
+        <img src={greenShape} alt="greenshape" className="green-svg"></img>
+        <img src={blueShape} alt="blueshape" className="blue-svg"></img>
+        
     </section>
 }
 
