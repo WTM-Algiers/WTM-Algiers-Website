dcd677824b627b8c5a8afdea17dd5d14e795a886
Abderraouf GATTAL <gattal.raouf@gmail.com>
2019-12-31 20:46:43 +0100

    fix the background

--- src/components/EventSection.js
+++ src/components/EventSection.js
@@ -6,19 +6,21 @@ import './EventSection.css'
 
 const EventSection = () => {
     return (
-        <div className="eventSection">
-            <div className="section-content-event">
-                <h1 className="title-section"><span id="our">OUR</span> EVENTS</h1>
-                <div className="section-container">
-                    <div className="slideshow">
-                        <EventSlideShow />
+        <div>
+            <div className="eventSection">
+                <div className="section-content-event">
+                    <h1 className="title-section"><span id="our">OUR</span> EVENTS</h1>
+                    <div className="section-container">
+                        <div className="slideshow">
+                            <EventSlideShow />
+                        </div>
                     </div>
-                </div>
-                <div className="green-svg">
-                    <img src={greenShape} alt="greenshape" />
-                </div>
-                <div className="blue-svg">
-                    <img src={blueShape} alt="blueshape" />
+                    <div className="green-svg">
+                        <img src={greenShape} alt="greenshape" />
+                    </div>
+                    <div className="blue-svg">
+                        <img src={blueShape} alt="blueshape" />
+                    </div>
                 </div>
             </div>
         </div>
