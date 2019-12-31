a1140c3d5b29ffc6357a971beadeb3396e4b3276
Abderraouf GATTAL <gattal.raouf@gmail.com>
2019-12-31 18:05:34 +0100

    fix the slider component styling issues

--- src/components/EventSection.js
+++ src/components/EventSection.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import EventSlideShow from './EventSlideShow'
 import greenShape from '../images/shapes/green-shape-1.svg'
-
 import blueShape from '../images/shapes/blue-shape-1.svg'
 import './EventSection.css'
-const EventSection =() => {
-    return <section className="eventSection">
-        <h1 className="title-section"><span id ="our">OUR</span> EVENTS</h1>
-        <div className ="section-container">
-            <div className="slideshow">
-                <EventSlideShow></EventSlideShow>
-            </div>   
+
+const EventSection = () => {
+    return (
+        <div className="eventSection">
+            <div className="section-content-event">
+                <h1 className="title-section"><span id="our">OUR</span> EVENTS</h1>
+                <div className="section-container">
+                    <div className="slideshow">
+                        <EventSlideShow />
+                    </div>
+                </div>
+                <img src={greenShape} alt="greenshape" className="green-svg" />
+                <img src={blueShape} alt="blueshape" className="blue-svg" />
+            </div>
         </div>
-        <img src={greenShape} alt="greenshape" className="green-svg"></img>
-        <img src={blueShape} alt="blueshape" className="blue-svg"></img>
-        
-    </section>
+    )
 }
 
 export default EventSection
--- src/components/EventSlideShow.js
+++ src/components/EventSlideShow.js
@@ -7,68 +7,70 @@ import Slider from 'react-animated-slider'
 import 'react-animated-slider/build/horizontal.css';
 import loadgif from '../images/loading.gif'
 import './SlideShow.css'
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const eventData =[
+const eventData = [
     {
-        title:"IWD'20",
-        image:img_bg,
-        description :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
-        
+        title: "IWD'20",
+        image: img_bg,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
+
     },
     {
-        title:"IWD'21",
-        image:img_bg,
-        description :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
-        
+        title: "IWD'21",
+        image: img_bg,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
+
     },
     {
-        title:"IWD'22",
-        image:img_bg,
-        description :"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
-        
-    }, 
+        title: "IWD'22",
+        image: img_bg,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis.Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Risus nec feugiat in fermentum…."
+
+    },
 
 
 ]
 
 
-const EventSlideShow = ()=> {
-    const [loading,setLoading] = useState(true);
+const EventSlideShow = () => {
+    const [loading, setLoading] = useState(true);
     /// Loading data
-    const [data,setData] = useState([]);
-    
-    useEffect(()=>{
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
         // load events data from markdown instead of eventData object 
-        setTimeout(()=> {
+        setTimeout(() => {
             setData(eventData)
             setLoading(false)
-        },4000)
-        
-    },[]) 
+        }, 4000)
+
+    }, [])
     const events = data.map(event => {
-        return <EventSlide image={event.image} title={event.title} description={event.description}></EventSlide>
+        return <EventSlide image={event.image} title={event.title} description={event.description} />
     })
-      return <div style={{height:'100%',marginBottom :'5%'}}>
-          <Slider
-          duration={500}
-      classNames ={{
-          slider :'slider',
-          track :'track',
-        }}
-        
-        nextButton={<img className="slideButton" src={next} title="next event"></img>}
-        
-        previousButton={<img className="slideButton" src={previous} title="previous event"></img>}
-      >
-         
-        { (!loading)? events.map((slide, index) => <div key={index}>
-            {slide}
-        </div>) : <div key={0}>
-                <img className ="loading" src={loadgif} width={100} height={100}></img>
-            </div>}
-      </Slider>
-          </div>
+    return (
+        <div className="SlideShowContent">
+            <Slider
+                duration={500}
+                classNames={{
+                    slider: 'slider',
+                    track: 'track',
+                }}
+
+                nextButton={<img className="slideButton" src={next} title="next event" />}
+
+                previousButton={<img className="slideButton" src={previous} title="previous event" />}
+            >
+
+                {(!loading) ? events.map((slide, index) => <div key={index}>
+                    {slide}
+                </div>) : <div key={0}>
+                        <img className="loading" src={loadgif} width={100} height={100} />
+                    </div>}
+            </Slider>
+        </div>
+    )
 }
 
 
