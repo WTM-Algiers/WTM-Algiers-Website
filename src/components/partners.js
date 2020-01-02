55e71df065e16db90d59eb65c895bc480ff533b6
Abderraouf GATTAL <gattal.raouf@gmail.com>
2020-01-02 16:01:19 +0100

    build the partners section

---
+++ src/components/partners.js
@@ -0,0 +1,35 @@
+import React from "react";
+import backgroundShape from "../images/shapes/white-shape-1.svg";
+import logo from "../images/shapes/GDGAlgiers-logo.svg";
+import glogo from "../images/gatsby-icon.png";
+
+
+import './partner.css';
+
+const partner = () => (
+    <div className="section-holder">
+        <div className="section-title">
+            <span id="our">OUR</span> PARTNERS
+        </div>
+
+        <div className="partner-holder">
+            <div>
+                <div className="partner-logo">
+                    <img src={logo} alt="" />
+                </div>
+                <img src={backgroundShape} className="rotate" alt="background shape" />
+            </div>
+
+            <div>
+                <div className="partner-logo">
+
+                </div>
+                <img src={backgroundShape} alt="background shape" />
+            </div>
+        </div>
+
+    </div>
+);
+
+
+export default partner;
