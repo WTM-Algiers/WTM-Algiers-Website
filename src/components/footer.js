d830612d871322d60458e3a1b3273cfb3e948648
Redha Amirouche <hm_amirouche@esi.dz>
2021-02-18 13:16:35 +0100

    add dynamic current year to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,23 +4,29 @@ import wtmlogo from "../images/logo/WTM logo.svg";
 import footerImg from "../images/footer.svg";
 import './footer.css';
 
-const Footer = () => (
-  <footer className="footer-container">
-    <div className="footer-svg">
-      <img src={footerImg} alt="footer svg" />
-    </div>
-    <div className="footer-box">
-      <div className="logo-f">
-        <img src={wtmlogo} alt="WomenTechmakers Algiers logo" />
-      </div>
-      <div className="copyrights">&copy; 2019 WTM Algiers All Rights Reserved.</div>
-      <div className="social">
-        <SM />
-      </div>
-    </div>
-    <div className="footer-down"></div>
-  </footer>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
 
+  return (
+  <footer className="footer-container">
+		<div className="footer-svg">
+			<img src={footerImg} alt="footer svg" />
+		</div>
+		<div className="footer-box">
+			<div className="logo-f">
+				<img src={wtmlogo} alt="WomenTechmakers Algiers logo" />
+			</div>
+			<div className="copyrights">
+				&copy; {currentYear} WTM Algiers All Rights
+				Reserved.
+			</div>
+			<div className="social">
+				<SM />
+			</div>
+		</div>
+		<div className="footer-down"></div>
+	</footer>
+);
+}
 
 export default Footer;
